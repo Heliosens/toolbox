e2657178737085959559ffa75a1d680245d17f25
Sylvie-B <unsolo@hotmail.fr>
2022-03-15 17:17:32 +0100

    setting page

--- View/base.php
+++ View/base.php
@@ -6,20 +6,52 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <title>Les ados de l'Avesnois</title>
+    <title>Ados en Avesnois</title>
 </head>
 <body>
     <header>
-        <img src="/img/logo.jpg" alt="logo de La 3CA">
+        <div>
+            <img src="/img/logo.jpg" alt="logo de La 3CA">
+            <span>Communauté de Commune du Coeur de l'Avesnois</span>
+        </div>
         <nav>
             <ul>
-                <li><a href="">Accueil</a></li>
-                <li><a href="">Activités sportives</a></li>
-                <li><a href="">Activités culturelles</a></li>
-                <li><a href="">Le numérique</a></li>
-                <li><a href="">Utile</a></li>
+                <li><a href="">ACCUEIL</a></li>
+                <li><a href="">ACTIVITES SPORTIVES</a></li>
+                <li><a href="">SORTIES CULTURELLES</a></li>
+                <li><a href="">LE NUMERIQUE</a></li>
+                <li><a href="">UTILE</a></li>
+                <li><a href="">
+                        <img id="glass" src="/img/blue-glass.png" alt="">
+                    </a></li>
+                <li><a href="">
+                    <img id="logoUser" src="/img/blueUser.png" alt="">
+                </a></li>
             </ul>
         </nav>
     </header>
+    <section>
+        <div id="sport">
+            <article>
+                <a href=""></a>
+                <span>ACTIVITES SPORTIVES</span>
+            </article>
+        </div>
+        <div id="culture">
+            <article>
+                <span>SORTIES CULTURELLES</span>
+            </article>
+        </div>
+        <div id="numerical">
+            <article>
+                <span>LE NUMERIQUE</span>
+            </article>
+        </div>
+        <div id="useful">
+            <article>
+                <span>UTILE</span>
+            </article>
+        </div>
+    </section>
 </body>
 </html>
